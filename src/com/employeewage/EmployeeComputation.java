9ee90ecf3316e6dc16484046ef53dd3283240311
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-14 21:17:27 +0530

    calculate Wage

--- src/com/employeewage/EmployeeComputation.java
+++ src/com/employeewage/EmployeeComputation.java
@@ -3,15 +3,42 @@ import java.io.*;
 class EmployeeComputation
 {
 
+	private int perHour,workHour,dayHour,payment;
+	public static Random rand=new Random();
+
+	EmployeeComputation(int perHour, int workHour, int dayHour)
+	{
+		this.perHour=perHour;
+		this.workHour=workHour;
+		this.dayHour=dayHour;
+	}
+
+	public int getperHour()
+	{
+		return this.perHour;
+	}
+
+	public void setperHour(int wage)
+	{
+		this.perHour=wage;
+	}
+
 	public static boolean isPresent()
 	{
-		Random rand=new Random();
 		int random=rand.nextInt(50)%2;
 		return random==1?true:false; 
 	}
 
+	public int calPayment()
+	{
+		this.payment=this.perHour*this.workHour;
+		return this.payment; 
+	}
+
 	public static void main(String[] args)
-		{
+	{
+		EmployeeComputation appObj=new EmployeeComputation(20,rand.nextInt(10),8);
+		System.out.println(appObj.calPayment());
 		System.out.println("Computation Program");
 			if(isPresent())
 			{

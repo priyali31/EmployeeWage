92a71416432313cd49aeea1827a94583e3fe6399
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-15 23:20:42 +0530

    List and HashMap of company

--- src/com/employeewage/service/EmployeeComputationService.java
+++ src/com/employeewage/service/EmployeeComputationService.java
@@ -1,6 +1,7 @@
 package com.employeewage.service;
 import com.employeewage.model.Employee;
 import com.employeewage.model.company;
+
 public interface EmployeeComputationService
 {
 
@@ -8,9 +9,5 @@ public interface EmployeeComputationService
 	public int randomWorkHours(String companyName);
 	public void addCompanyWithEmployees(String companyName,int ratePerHour,int workingDays,int maxHoursPerMonth);
 	public company findCompany(String companyName);
-	/*public int calculatePay(Employee emp);
-	public String getTypeOfEmployee(Employee emp);
-	public int monthlyCalculatePay(Employee emp);
-	public int tillHundredCalculatePay(Employee emp);*/
 
 }

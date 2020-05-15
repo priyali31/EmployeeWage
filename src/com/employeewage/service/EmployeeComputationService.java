40ed40288cdd7484580949f75cc611af93575b7e
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-15 23:00:17 +0530

    interface on service

--- src/com/employeewage/service/EmployeeComputationService.java
+++ src/com/employeewage/service/EmployeeComputationService.java
@@ -1,10 +1,13 @@
 package com.employeewage.service;
 import com.employeewage.model.Employee;
-
+import com.employeewage.model.company;
 public interface EmployeeComputationService
 {
 
 	public boolean isPresent(Employee emp);
+	public int randomWorkHours(String companyName);
+	public void addCompanyWithEmployees(String companyName,int ratePerHour,int workingDays,int maxHoursPerMonth);
+	public company findCompany(String companyName);
 	/*public int calculatePay(Employee emp);
 	public String getTypeOfEmployee(Employee emp);
 	public int monthlyCalculatePay(Employee emp);

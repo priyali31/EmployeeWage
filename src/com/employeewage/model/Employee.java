6868a41fbeae9416f24c7014901a7289147497d5
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-15 21:37:28 +0530

    building wage

---
+++ src/com/employeewage/model/Employee.java
@@ -0,0 +1,54 @@
+package com.employeewage.model;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee
+{
+	private String employeeWas,companyName;
+	private int payment;
+	private List<Integer> dw;
+
+	public Employee(ArrayList<Integer> dw)
+	{
+		this.dw=dw;
+	}
+
+	public String getEmployeeWas()
+	{
+		return this.employeeWas;
+	}
+
+	public void setEmployeeWas(String EmployeeWas)
+	{
+		this.employeeWas=employeeWas;
+	}
+
+	public void setCompanyName(String companyName)
+	{
+		this.companyName=companyName;
+	}
+
+	public int getPayment()
+	{
+		return this.payment;
+	}
+
+	public void setPayment(int payment)
+	{
+		this.payment=payment;
+	}
+
+	public List<Integer> getDw()
+	{
+		return dw;
+	}
+
+	public void setDw(List<Integer> dw)
+	 {
+		this.dw=dw;
+	}
+	@Override
+	public String toString()
+	{
+		return "Employee is  " + this.employeeWas + "Overall Payment of Employee " + this.payment;
+	}
